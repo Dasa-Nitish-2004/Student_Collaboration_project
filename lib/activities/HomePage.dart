faa6bb9b17213bbd4d1a7e4b6fb69973cd08d308
dasa nitish <dasanitish108@gmail.com>
2024-06-08 10:24:35 +0530

    home_navigator created

--- lib/activities/HomePage.dart
+++ lib/activities/HomePage.dart
@@ -1,19 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:scolab/activities/skillsPage.dart';
 
 class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Scaffold(
+      body: Text('Home Page',
+          style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    ),
+    Text('Search Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    SkillPage(title: "skill info"),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text("Next_page"),
-        ),
+      drawer: Drawer(),
+      appBar: AppBar(
+          title: SizedBox(
+            height: 40,
+            child: TextField(
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search_rounded),
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(26),
+                  ),
+                  alignLabelWithHint: true,
+                  hintText: "Search"),
+              onTap: () {},
+            ),
+          ),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "Home",
+                backgroundColor: Color.fromARGB(255, 149, 117, 205)),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: 'Search',
+                backgroundColor: Color.fromARGB(255, 121, 80, 190)),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+              backgroundColor: Colors.deepPurple,
+            ),
+          ],
+          type: BottomNavigationBarType.shifting,
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.black,
+          iconSize: 30,
+          onTap: _onItemTapped,
+          elevation: 5),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,294 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/widgets.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
-// import 'package:provider/provider.dart';
-// import 'package:scolab/DatabaseService/databaseServices.dart';
-// import 'package:scolab/activities/HomePage.dart';
-// import 'package:scolab/activities/skillsPage.dart';
-// import 'package:mongo_dart/mongo_dart.dart' as mongo;
-// import 'package:shared_preferences/shared_preferences.dart';
-
-// void main() {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   var loggedin = "";
-
-//   MyApp({super.key}) {
-//     checkLog();
-//     print("constructor called ${loggedin}");
-//   }
-
-//   void checkLog() async {
-//     var prefs = await SharedPreferences.getInstance();
-//     if (prefs.getString("email") == null || prefs.getString("email") == "") {
-//       loggedin = "";
-//     } else {
-//       print(
-//           "saved email found^^^^^^^^^^^^^^^^${prefs.getString("email")}^^^^^^^^^^^^^");
-//       loggedin = await prefs.getString("email")!;
-//       if (await MongoDb()
-//           .checkUserExists(await MongoDb().getConnection(), loggedin)) {
-//         await prefs.setString('email', "");
-//         loggedin = "";
-//       }
-//     }
-//     print("check completed");
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         debugShowCheckedModeBanner: false,
-//         theme: ThemeData(
-//           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//           useMaterial3: true,
-//         ),
-//         home: loggedin == "" ? signInPage() : HomeScreen());
-//   }
-// }
-
-// class signInPage extends StatefulWidget {
-//   signInPage({super.key});
-
-//   @override
-//   State<signInPage> createState() => _signInPageState();
-// }
-
-// class _signInPageState extends State<signInPage> {
-//   late mongo.Db db;
-//   List data = [];
-//   // late final SharedPreferences prefs;
-
-//   void connect() async {
-//     db = await MongoDb().getConnection();
-//     fetchData();
-//   }
-
-//   @override
-//   void dispose() {
-//     // TODO: implement dispose
-//     db.close();
-//     super.dispose();
-//   }
-
-//   void fetchData() async {
-//     data = await MongoDb().getIds(db);
-//     // print(data);
-//   }
-
-//   late GoogleSignIn _googleSignIn;
-
-//   Widget login_space = Container();
-//   var email = "";
-
-//   registerUser() async {
-//     _googleSignIn = GoogleSignIn();
-//     try {
-//       var result = await _googleSignIn.signIn();
-//       email = result!.email;
-//       MongoDb().addUser(db, email);
-//       signOut();
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(
-//           content: Text("successfull registered in $email"),
-//           duration: Duration(seconds: 2),
-//         ),
-//       );
-//     } catch (error) {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(
-//           content: Text("Unsuccessfull to register :$email try again"),
-//           duration: Duration(seconds: 2),
-//         ),
-//       );
-//       signOut();
-//       print(error);
-//     } finally {
-//       setState(() {});
-//     }
-//   }
-
-//   signIn() async {
-//     _googleSignIn = GoogleSignIn();
-//     try {
-//       var result = await _googleSignIn.signIn();
-//       email = result!.email;
-//       showDialog(
-//         context: context,
-//         builder: (context) {
-//           return Center(
-//             child: CircularProgressIndicator(),
-//           );
-//         },
-//       );
-//       if (data.contains(email) ||
-//           !await MongoDb()
-//               .checkUserExists(await MongoDb().getConnection(), email)) {
-//         var prefs = await SharedPreferences.getInstance();
-//         await prefs.setString('email', email);
-//         Navigator.pop(context);
-//         ScaffoldMessenger.of(context).showSnackBar(
-//           SnackBar(
-//             content: Text("successfull signed in $email"),
-//             duration: Duration(seconds: 2),
-//           ),
-//         );
-//         Navigator.of(context).pushReplacement(MaterialPageRoute(
-//           builder: (context) {
-//             return SkillPage(
-//               title: 'Info & Skill',
-//             );
-//           },
-//         ));
-//       } else {
-//         signOut();
-//         Navigator.pop(context);
-//         ScaffoldMessenger.of(context).showSnackBar(
-//           SnackBar(
-//             content: Text("failed to Sign In please Register the account"),
-//             duration: Duration(seconds: 2),
-//           ),
-//         );
-//         email = "";
-//       }
-//     } catch (error) {
-//       signOut();
-//       print("error : ***${error}***");
-//       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-//         content: Text("Check internet connectivity"),
-//         duration: Duration(seconds: 3),
-//       ));
-//     } finally {
-//       setState(() {});
-//     }
-//   }
-
-//   signOut() {
-//     _googleSignIn.signOut();
-//   }
-
-//   @override
-//   void initState() {
-//     login_space = signInEmail();
-//     connect();
-//     super.initState();
-//   }
-
-//   var sharedPref = "";
-
-//   void lode() async {
-//     var pref = await SharedPreferences.getInstance();
-//     sharedPref = pref.getString("email")!;
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     lode();
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         title: Text("SCOLAB"),
-//       ),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Text("<<<<<<${sharedPref}>>>>>>"),
-//           SizedBox(
-//             height: 20,
-//           ),
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               ElevatedButton(
-//                   onPressed: () {
-//                     setState(() {
-//                       login_space = signInEmail();
-//                     });
-//                   },
-//                   child: Text("Sign In")),
-//               SizedBox(
-//                 width: 20,
-//               ),
-//               ElevatedButton(
-//                   onPressed: () {
-//                     setState(() {
-//                       login_space = registerEmail();
-//                     });
-//                   },
-//                   child: Text("Register")),
-//             ],
-//           ),
-//           SizedBox(
-//             height: 20,
-//           ),
-//           login_space,
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget registerEmail() {
-//     return Container(
-//       width: double.infinity,
-//       margin: EdgeInsets.all(8.0),
-//       decoration: BoxDecoration(
-//         border: Border.all(
-//           width: 2,
-//           color: Colors.deepPurple,
-//         ),
-//         borderRadius: BorderRadius.circular(26),
-//       ),
-//       child: Column(
-//         children: [
-//           Image.asset(
-//             "assets/imgs/googleLogo.png",
-//             height: 200,
-//             width: double.infinity,
-//           ),
-//           ElevatedButton(
-//               onPressed: registerUser, child: Text("Register with Mail")),
-//           SizedBox(
-//             height: 20,
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget signInEmail() {
-//     return Container(
-//       width: double.infinity,
-//       margin: EdgeInsets.all(8.0),
-//       decoration: BoxDecoration(
-//         border: Border.all(
-//           width: 2,
-//           color: Colors.deepPurple,
-//         ),
-//         borderRadius: BorderRadius.circular(26),
-//       ),
-//       child: Column(
-//         children: [
-//           Image.asset(
-//             "assets/imgs/googleLogo.png",
-//             height: 200,
-//             width: double.infinity,
-//           ),
-//           ElevatedButton(
-//               onPressed: signIn, child: Text("Sign with Registered Mail")),
-//           SizedBox(
-//             height: 20,
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// // const MyHomePage(title: 'Flutter Demo Home Page'),
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -374,17 +83,12 @@ class _signInPageState extends State<signInPage> {
 
   void connect() async {
     db = await MongoDb().getConnection();
-    fetchData();
   }
 
   @override
   void dispose() {
     db.close();
     super.dispose();
-  }
-
-  void fetchData() async {
-    data = await MongoDb().getIds(db);
   }
 
   registerUser() async {
@@ -427,9 +131,8 @@ class _signInPageState extends State<signInPage> {
           );
         },
       );
-      if (data.contains(email) ||
-          !await MongoDb()
-              .checkUserExists(await MongoDb().getConnection(), email)) {
+      if (!await MongoDb()
+          .checkUserExists(await MongoDb().getConnection(), email)) {
         var prefs = await SharedPreferences.getInstance();
         await prefs.setString('email', email);
         Navigator.pop(context);
@@ -471,6 +174,7 @@ class _signInPageState extends State<signInPage> {
 
   signOut() {
     _googleSignIn.signOut();
+    _googleSignIn.disconnect();
   }
 
   @override
@@ -498,7 +202,6 @@ class _signInPageState extends State<signInPage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("<<<<<<${sharedPref}>>>>>>"),
           SizedBox(
             height: 20,
           ),
@@ -511,7 +214,13 @@ class _signInPageState extends State<signInPage> {
                       login_space = signInEmail();
                     });
                   },
-                  child: Text("Sign In")),
+                  style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                          Color.fromARGB(255, 222, 204, 254))),
+                  child: Text(
+                    "Sign In",
+                    style: TextStyle(color: Colors.black),
+                  )),
               SizedBox(
                 width: 20,
               ),
@@ -521,7 +230,13 @@ class _signInPageState extends State<signInPage> {
                       login_space = registerEmail();
                     });
                   },
-                  child: Text("Register")),
+                  style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                          Color.fromARGB(255, 222, 204, 254))),
+                  child: Text(
+                    "Register",
+                    style: TextStyle(color: Colors.black),
+                  )),
             ],
           ),
           SizedBox(
